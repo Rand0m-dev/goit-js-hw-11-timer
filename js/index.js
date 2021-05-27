d6f88f342a15a8ff0be7a7cdd7b1aefa6882c67f
Ihor Kirienko <flesh0099@gmail.com>
2021-05-27 23:56:01 +0300

    ДЗ№11.3

---
+++ js/index.js
@@ -0,0 +1,58 @@
+const ulList = document.querySelector("#timer-1");
+const spanElDays = document.querySelector('.value[data-value="days"]');
+const spanElHours = document.querySelector('.value[data-value="hours"]');
+const spanElMins = document.querySelector('.value[data-value="mins"]');
+const spanElSecs = document.querySelector('.value[data-value="secs"]');
+
+class CountdownTimer {
+  constructor({ targetDate, selector, updateFace }) {
+    this.targetDate = targetDate;
+    this.value = selector;
+    this.updateFace = updateFace;
+  }
+
+  updateCount() {
+    const currentTime = Date.now();
+    const timeToFinish = this.targetDate - currentTime;
+    const time = this.getTimeToTargetDate(timeToFinish);
+
+    this.updateFace(time);
+  }
+
+  start() {
+    this.updateCount();
+
+    setInterval(() => {
+      this.updateCount();
+    }, 1000);
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  getTimeToTargetDate(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+}
+
+const countdownTimer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Augest 07, 2021"),
+  updateFace: updateCountdownTimer,
+});
+
+countdownTimer.start();
+
+function updateCountdownTimer({ days, hours, mins, secs }) {
+  spanElDays.textContent = `${days}`;
+  spanElHours.textContent = `${hours}`;
+  spanElMins.textContent = `${mins}`;
+  spanElSecs.textContent = `${secs}`;
+}
